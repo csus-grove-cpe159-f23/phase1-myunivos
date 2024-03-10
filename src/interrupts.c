1aa7ab787c3d4cbc7781c42f75d199cc481bf4f8
Sameer Shaik <125149832+sameers22@users.noreply.github.com>
2024-03-09 19:29:25 -0800

    Update Draft2 interrupts.c

--- src/interrupts.c
+++ src/interrupts.c
@@ -2,200 +2,155 @@
  * CPE/CSC 159 - Operating System Pragmatics
  * California State University, Sacramento
  *
- * Interrupt handling functions
+ * Timer Implementation
  */
-#include <spede/machine/io.h>
-#include <spede/machine/proc_reg.h>
-#include <spede/machine/seg.h>
+#include <spede/stdio.h>
 #include <spede/string.h>
+#include <spede/flames.h> // For inb and outb functions
 
-#include "kernel.h"
 #include "interrupts.h"
-
-// Maximum number of ISR handlers
-#define IRQ_MAX      0xf0
-
-// PIC Definitions
-#define PIC1_BASE   0x20            // base address for PIC primary controller
-#define PIC2_BASE   0xa0            // base address for PIC secondary controller
-#define PIC1_CMD    PIC1_BASE       // address for issuing commands to PIC1
-#define PIC1_DATA   (PIC1_BASE+1)   // address for setting data for PIC1
-#define PIC2_CMD    PIC2_BASE       // address for issuing commands to PIC2
-#define PIC2_DATA   (PIC2_BASE+1)   // address for setting data for PIC2
-
-#define PIC_EOI     0x20            // PIC End-of-Interrupt command
-
-// Interrupt descriptor table
-struct i386_gate *idt = NULL;
-
-// Interrupt handler table
-// Contains an array of function pointers associated with
-// the various interrupts to be handled
-void (*irq_handlers[IRQ_MAX])();
+#include "kernel.h"
+#include "queue.h"
+#include "timer.h"
 
 /**
- * Enable interrupts with the CPU
+ * Data structures
  */
-void interrupts_enable(void) {
-    kernel_log_trace("interrupts: enabling");
-    asm("sti");
-}
+// Timer data structure
+typedef struct timer_t {
+    void (*callback)(); // Function to call when the interval occurs
+    int interval;       // Interval in which the timer will be called
+    int repeat;         // Indicate how many intervals to repeat (-1 should repeat forever)
+} timer_t;
 
 /**
- * Disable interrupts with the CPU
+ * Variables
  */
-void interrupts_disable(void) {
-    kernel_log_trace("interrupts: disabling");
-    asm("cli");
-}
+
+// Number of timer ticks that have occurred
+int timer_ticks;
+
+// Timers table; each item in the array is a timer_t struct
+timer_t timers[TIMERS_MAX];
+
+// Timer allocator; used to allocate indexes into the timers table
+queue_t timer_allocator;
 
 /**
- * Handles the specified interrupt by dispatching to the registered function
- * @param interrupt - interrupt number
- */
-void interrupts_irq_handler(int irq) {
-    if (irq < 0 || irq >= IRQ_MAX) {
-        kernel_panic("interrupts: Invalid IRQ %d (0x%02x)", irq, irq);
-        return;
-    }
-
-    if (irq_handlers[irq] == NULL) {
-        kernel_panic("interrupts: No handler registered for IRQ %d (0x%02x)", irq, irq);
-        return;
-    }
-
-    irq_handlers[irq]();
-
-    /* If the IRQ originates from the PIC, dismiss the IRQ */
-    if (irq >= 0x20 && irq <= 0x2F) {
-        pic_irq_dismiss(irq - 0x20);
-    }
-}
-
-/*
- * Registers the appropriate IDT entry and handler function for the
- * specified interrupt.
+ * Registers a new callback to be called at the specified interval
+ * @param func_ptr - function pointer to be called
+ * @param interval - number of ticks before the callback is performed
+ * @param repeat   - Indicate how many intervals to repeat (-1 should repeat forever)
  *
- * @param interrupt - interrupt number
- * @param entry - the function to run when the interrupt occurs
- * @param handler - the function to be called to process the the interrupt
+ * @return the allocated timer id or -1 for errors
  */
-void interrupts_irq_register(int irq, void (*entry)(), void (*handler)()) {
-    if (irq < 0 || irq >= IRQ_MAX) {
-        kernel_panic("interrupts: Invalid IRQ %d (0x%02x)", irq, irq);
-        return;
+int timer_callback_register(void (*func_ptr)(), int interval, int repeat) {
+    int timer_id = -1;
+
+    if (!func_ptr) {
+        kernel_log_error("timer: invalid function pointer");
+        return -1;
     }
 
-    if (!entry) {
-        kernel_panic("interrupts: Invalid IDT entry for IRQ %d (0x%02x)", irq, irq);
-        return;
+    // Obtain a timer id
+    if (queue_out(&timer_allocator, &timer_id) != 0) {
+        kernel_log_error("timer: unable to allocate a timer");
+        return -1;
     }
 
-    if (!handler) {
-        kernel_panic("interrupts: Invalid handler for IRQ %d (0x%02x)", irq, irq);
-        return;
-    }
+    // Set the callback function for the timer
+    timers[timer_id].callback = func_ptr;
+    // Set the interval value for the timer
+    timers[timer_id].interval = interval;
+    // Set the repeat value for the timer
+    timers[timer_id].repeat = repeat;
 
-    // Add the entry to the IDT
-    fill_gate(&idt[irq], (int)entry, get_cs(), ACC_INTR_GATE, 0);
-    kernel_log_debug("interrupts: IRQ %d (0x%02x) IDT entry added", irq, irq);
-
-    /* Add the ISR handler to the table */
-    irq_handlers[irq] = handler;
-    kernel_log_debug("interrupts: IRQ %d (0x%02x) handler added", irq, irq);
-
-    /* If the interrupt originates from the PIC, enable IRQs */
-    if (irq >= 0x20 && irq <= 0x2F) {
-        pic_irq_enable(irq);
-    }
-
-    kernel_log_info("interrupts: IRQ %d (0x%02x) registered)", irq, irq);
+    return timer_id;
 }
 
 /**
- * Enables the specified IRQ on the PIC
+ * Unregisters the specified callback
+ * @param id
  *
- * @param irq - IRQ that should be enabled
- * @note IRQs > 0xf will be remapped
+ * @return 0 on success, -1 on error
  */
-void pic_irq_enable(int irq) {
-    unsigned short port;
-    unsigned char mask;
-
-    if (irq < 8) {
-        port = PIC1_DATA;
-    } else {
-        port = PIC2_DATA;
-        irq -= 8;
+int timer_callback_unregister(int id) {
+    if (id < 0 || id >= TIMERS_MAX) {
+        kernel_log_error("timer: callback id out of range: %d", id);
+        return -1;
     }
 
-    mask = inb(port) & ~(1 << irq);
-    outb(port, mask);
+    // Clear the timer entry
+    memset(&timers[id], 0, sizeof(timer_t));
+
+    if (queue_in(&timer_allocator, id) != 0) {
+        kernel_log_error("timer: unable to queue timer entry back to allocator");
+        return -1;
+    }
+
+    return 0;
 }
 
 /**
- * Disables the specified IRQ via the PIC
+ * Returns the number of ticks that have occurred since startup
  *
- * @param irq - IRQ that should be disabled
+ * @return timer_ticks
  */
-void pic_irq_disable(int irq) {
-    unsigned short port;
-    unsigned char mask;
-
-    if (irq < 8) {
-        port = PIC1_DATA;
-    } else {
-        port = PIC2_DATA;
-        irq -= 8;
-    }
-
-    mask = inb(port) | (1 << irq);
-    outb(port, mask);
+int timer_get_ticks() {
+    return timer_ticks;
 }
 
 /**
- * Queries if the given IRQ is enabled on the PIC
+ * Timer IRQ Handler
  *
- * @param irq - IRQ to check
- * @return - 1 if enabled, 0 if disabled
+ * Should perform the following:
+ *   - Increment the timer ticks every time the timer occurs
+ *   - Handle each registered timer
+ *     - If the interval is hit, run the callback function
+ *     - Handle timer repeats
  */
-int pic_irq_enabled(int irq) {
-    unsigned short port;
-    unsigned char mask;
+void timer_irq_handler(void) {
+    // Increment the timer_ticks value
+    timer_ticks++;
 
-    if (irq < 8) {
-        port = PIC1_DATA;
-    } else {
-        port = PIC2_DATA;
-        irq -= 8;
+    // Iterate through the timers table
+    for (int i = 0; i < TIMERS_MAX; i++) {
+        if (timers[i].callback) {
+            if (timer_ticks % timers[i].interval == 0) {
+                timers[i].callback();
+                if (timers[i].repeat > 0) {
+                    timers[i].repeat--;
+                    if (timers[i].repeat == 0) {
+                        timer_callback_unregister(i);
+                    }
+                }
+            }
+        }
     }
-
-    mask = inb(port);
-    return (mask & (1 << irq)) ? 1 : 0;
 }
 
 /**
- * Dismisses an interrupt by sending the EOI command to the appropriate
- * PIC device(s). If the IRQ is associated with the secondary PIC, the
- * EOI command must be issued to both since the PICs are daisy-chained.
- *
- * @param irq - IRQ to be dismissed
+ * Initializes timer related data structures and variables
  */
-void pic_irq_dismiss(int irq) {
-    if (irq >= 8) {
-        outb(PIC2_CMD, PIC_EOI);
+void timer_init(void) {
+    kernel_log_info("Initializing timer");
+
+    // Set the starting tick value
+    timer_ticks = 0;
+
+    // Initialize the timers data structures
+    for (int i = 0; i < TIMERS_MAX; i++) {
+        memset(&timers[i], 0, sizeof(timer_t));
     }
-    outb(PIC1_CMD, PIC_EOI);
-}
 
-/**
- * Interrupt initialization
- */
-void interrupts_init() {
-    kernel_log_info("Initializing Interrupts");
+    // Initialize the timer callback allocator queue
+    queue_init(&timer_allocator);
 
-    // Obtain the IDT base address
-    idt = get_idt_base();
+    // Populate items into the allocator queue
+    for (int i = 0; i < TIMERS_MAX; i++) {
+        queue_in(&timer_allocator, i);
+    }
 
-    memset(irq_handlers, 0, sizeof(irq_handlers));
+    // Register the Timer IRQ with the isr_entry_timer and timer_irq_handler
+    interrupts_irq_register(IRQ_TIMER, timer_irq_handler);
 }
