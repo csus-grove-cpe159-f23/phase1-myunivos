55073224626991c8415ade540566220e84581ee5
Sameer Shaik <125149832+sameers22@users.noreply.github.com>
2024-03-12 17:01:27 -0700

    Update interrupts.c

--- src/interrupts.c
+++ src/interrupts.c
@@ -8,7 +8,6 @@
 #include <spede/machine/proc_reg.h>
 #include <spede/machine/seg.h>
 #include <spede/string.h>
-#include <spede/flames.h> // For inb and outb functions
 
 #include "kernel.h"
 #include "interrupts.h"
@@ -19,10 +18,10 @@
 // PIC Definitions
 #define PIC1_BASE   0x20            // base address for PIC primary controller
 #define PIC2_BASE   0xa0            // base address for PIC secondary controller
-#define PIC1_COMMAND_PORT 0x20
-#define PIC1_DATA_PORT 0x21
-#define PIC2_COMMAND_PORT 0xA0
-#define PIC2_DATA_PORT 0xA1
+#define PIC1_CMD    PIC1_BASE       // address for issuing commands to PIC1
+#define PIC1_DATA   (PIC1_BASE+1)   // address for setting data for PIC1
+#define PIC2_CMD    PIC2_BASE       // address for issuing commands to PIC2
+#define PIC2_DATA   (PIC2_BASE+1)   // address for setting data for PIC2
 
 #define PIC_EOI     0x20            // PIC End-of-Interrupt command
 
@@ -34,15 +33,6 @@ struct i386_gate *idt = NULL;
 // the various interrupts to be handled
 void (*irq_handlers[IRQ_MAX])();
 
-/**
- * Enable interrupts with the CPU
- */
-void interrupts_enable(void) {
-    kernel_log_trace("interrupts: enabling");
-    asm("sti");
-}
-
-
 // Define inline assembly functions for input and output operations
 static inline uint8_t inb(uint16_t port) {
     uint8_t data;
@@ -52,6 +42,14 @@ static inline uint8_t inb(uint16_t port) {
 
 static inline void outb(uint16_t port, uint8_t data) {
     asm volatile("outb %0, %1" :: "a"(data), "Nd"(port));
+}
+
+/**
+ * Enable interrupts with the CPU
+ */
+void interrupts_enable(void) {
+    kernel_log_trace("interrupts: enabling");
+    asm("sti");
 }
 
 /**
@@ -125,6 +123,17 @@ void interrupts_irq_register(int irq, void (*entry)(), void (*handler)()) {
     kernel_log_info("interrupts: IRQ %d (0x%02x) registered)", irq, irq);
 }
 
+// Define inline assembly functions for input and output operations
+static inline uint8_t inb(uint16_t port) {
+    uint8_t data;
+    asm volatile("inb %1, %0" : "=a"(data) : "Nd"(port));
+    return data;
+}
+
+static inline void outb(uint16_t port, uint8_t data) {
+    asm volatile("outb %0, %1" :: "a"(data), "Nd"(port));
+}
+
 /**
  * Enables the specified IRQ on the PIC
  *
@@ -132,18 +141,35 @@ void interrupts_irq_register(int irq, void (*entry)(), void (*handler)()) {
  * @note IRQs > 0xf will be remapped
  */
 void pic_irq_enable(int irq) {
-    uint8_t mask;
+    // Determine the PIC to be used for the given IRQ number
+    // Read the current mask
+    // Clear the associated bit in the mask to enable the IRQ
+    // Write the mask out to the PIC
     uint16_t port;
+    uint8_t mask;
 
-    if (irq < 8) {
-        port = PIC1_DATA_PORT;
-    } else {
-        port = PIC2_DATA_PORT;
-        irq -= 8;
+    if (irq < 0 || irq > 15) {
+        kernel_log_error("pic_irq_enable: Invalid IRQ %d", irq);
+        return;
     }
 
-    mask = inb(port) & ~(1 << irq);
-    outb(port, mask);
+    // Determine the PIC to be used for the given IRQ number
+    if (irq < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irq -= 8; // For the secondary PIC, IRQs start from 8
+    }
+
+    // Read the current mask
+    mask = inb(port);
+
+    // Clear the associated bit in the mask to enable the IRQ
+    mask &= ~(1 << irq);
+
+    // Write the mask out to the PIC
+    outb(mask, port);
+
 }
 
 /**
@@ -152,18 +178,34 @@ void pic_irq_enable(int irq) {
  * @param irq - IRQ that should be disabled
  */
 void pic_irq_disable(int irq) {
-    uint8_t mask;
+    // Determine the PIC to be used for the given IRQ number
+    // Read the current mask
+    // Set the associated bit in the mask to disable the IRQ
+    // Write the mask back to the PIC
     uint16_t port;
+    uint8_t mask;
 
-    if (irq < 8) {
-        port = PIC1_DATA_PORT;
-    } else {
-        port = PIC2_DATA_PORT;
-        irq -= 8;
+    if (irq < 0 || irq > 15) {
+        kernel_log_error("pic_irq_disable: Invalid IRQ %d", irq);
+        return;
     }
 
-    mask = inb(port) | (1 << irq);
-    outb(port, mask);
+    // Determine the PIC to be used for the given IRQ number
+    if (irq < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irq -= 8; // For the secondary PIC, IRQs start from 8
+    }
+
+    // Read the current mask
+    mask = inb(port);
+
+    // Set the associated bit in the mask to disable the IRQ
+    mask |= (1 << irq);
+
+    // Write the mask back to the PIC
+    outb(mask, port);
 }
 
 /**
@@ -173,19 +215,33 @@ void pic_irq_disable(int irq) {
  * @return - 1 if enabled, 0 if disabled
  */
 int pic_irq_enabled(int irq) {
-    uint8_t mask;
+    // Determine the PIC to be used and adjust the irq number
+    // Read the current mask from the data port
+    // check the associated bit and return if the IRQ is enabled
     uint16_t port;
+    uint8_t mask;
 
-    if (irq < 8) {
-        port = PIC1_DATA_PORT;
-    } else {
-        port = PIC2_DATA_PORT;
-        irq -= 8;
+    if (irq < 0 || irq > 15) {
+        kernel_log_error("pic_irq_enabled: Invalid IRQ %d", irq);
+        return 0;
     }
 
+    // Determine the PIC to be used for the given IRQ number
+    if (irq < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        irq -= 8; // For the secondary PIC, IRQs start from 8
+    }
+
+    // Read the current mask from the data port
     mask = inb(port);
+
+    // Check the associated bit and return if the IRQ is enabled
     return !(mask & (1 << irq));
+
 }
+
 /**
  * Dismisses an interrupt by sending the EOI command to the appropriate
  * PIC device(s). If the IRQ is assosciated with the secondary PIC, the
@@ -194,10 +250,20 @@ int pic_irq_enabled(int irq) {
  * @param irq - IRQ to be dismissed
  */
 void pic_irq_dismiss(int irq) {
-    if (irq >= 8) {
-        outb(PIC2_COMMAND_PORT, 0x20); // EOI to secondary PIC
+    // Send EOI to the secondary PIC, if needed
+    // Send EOI to the primary PIC, if needed
+    if (irq < 0 || irq > 15) {
+        kernel_log_error("pic_irq_dismiss: Invalid IRQ %d", irq);
+        return;
     }
-    outb(PIC1_COMMAND_PORT, 0x20); // EOI to primary PIC
+
+    // Send EOI to the secondary PIC, if needed
+    if (irq >= 8) {
+        outb(PIC_EOI, PIC2_CMD);
+    }
+
+    // Send EOI to the primary PIC
+    outb(PIC_EOI, PIC1_CMD);
 }
 
 /**
@@ -211,3 +277,6 @@ void interrupts_init() {
 
     memset(irq_handlers, 0, sizeof(irq_handlers));
 }
+
+interrupts_text_2.txt
+8 KB
