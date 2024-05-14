a3fc7ab9a36c42081f1422e77c53b618095768b8
FabriceYonkeu <141974181+fach08@users.noreply.github.com>
2024-05-13 20:39:54 -0700

    Update kproc.h from phase5-base

--- include/kproc.h
+++ include/kproc.h
@@ -33,7 +33,7 @@ typedef enum state_t {
     NONE,               // Process has no state (doesn't exist)
     IDLE,               // Process is idle (not scheduled)
     ACTIVE,             // Process is active (scheduled)
-    SLEEPING            // Process is sleeping (not scheduled)
+    SLEEPING,            // Process is sleeping (not scheduled)
     WAITING,            // Process is waiting (not scheduled)
 } state_t;
 
